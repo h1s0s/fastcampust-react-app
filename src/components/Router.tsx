a945a3597a3ed1b990ab8abc2d9e6f26fb8c150c
h1s0s <hs94122@naver.com>
2025-02-18 14:05:19 +0900

    2025-02-18

---
+++ src/components/Router.tsx
@@ -0,0 +1,27 @@
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Home from '../pages/home';
+import PostList from '../pages/post';
+import PostDetail from '../pages/post/detail';
+import PostNew from '../pages/post/new';
+import PostEdit from '../pages/post/edit';
+import ProfilePage from '../pages/profile';
+import LoginPage from '../pages/login';
+import SignupPage from '../pages/signup';
+
+export default function Router() {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/posts" element={<PostList />} />
+      <Route path="/posts/:id" element={<PostDetail />} />
+      <Route path="/posts/new" element={<PostNew />} />
+      <Route path="/posts/edit/:id" element={<PostEdit />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="*" element={<Navigate to ="/" />} />
+    </Routes>
+  </>
+  );
+}
